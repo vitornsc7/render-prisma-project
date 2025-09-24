428a7046819743ce6864000c260fdd31c4e27abf
Vitor Nascimento <vitornsc7@gmail.com>
2025-09-23 21:05:38 -0300

    add: health check

--- index.js
+++ index.js
@@ -19,6 +19,21 @@ app.post("/users", async (req, res) => {
     }
 });
 
+app.get("/health", (req, res) => {
+    res.status(200).json({ status: "OK", uptime: process.uptime() });
+});
+
+app.get("/user/:id", async (req, res) => {
+    const { id } = req.params;
+    const user = await prisma.user.findUnique({ where: { id: Number(id) } })
+
+    if (!user) {
+        return res.status(404).json({ error: "Usuário não encontrado." })
+    }
+
+    res.json(user);
+})
+
 // Get all users
 app.get("/users", async (req, res) => {
     try {
